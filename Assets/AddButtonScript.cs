c28fe1c0141b2903cf806ddae5057b33ec208e18
Gangadhara Matti <gangadharmatti.16@gmail.com>
2020-02-20 21:10:24 -0700

    Basic calculator

---
+++ Assets/AddButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AddButtonScript : MonoBehaviour
+{
+    public InputField num1;
+    public InputField num2;
+    public Text result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int first = int.Parse(num1.text);
+        int second = int.Parse(num2.text);
+        result.text = "sdfsdf";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/buttonHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Calculator;
+
+public class buttonHandler : MonoBehaviour
+{
+
+    public InputField number1;
+    public InputField number2;
+    
+    public Text resultText;
+
+    public Button addButtonClick;
+    public Button subButtonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        addButtonClick.onClick.AddListener(add);
+        subButtonClick.onClick.AddListener(sub);
+    }
+
+    public void add()
+    {
+        int a = int.Parse(number1.text);
+        int b = int.Parse(number2.text);
+        Calculate cal = new Calculate();
+        resultText.text = "Result : " + cal.Add(a, b).ToString();
+    }
+
+    public void sub()
+    {
+        int a = int.Parse(number1.text);
+        int b = int.Parse(number2.text);
+        Calculate cal = new Calculate();
+        resultText.text = "Result : " + cal.Sub(a, b).ToString();
+    }
+
+    void Update()
+    {
+    }
+}
